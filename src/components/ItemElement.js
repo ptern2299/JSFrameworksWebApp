cfeb60d183a8d40804cf3594ef0a0a0d3cce0613
pasht <p.tern2298@outlook.com>
2022-12-02 17:05:46 -0500

    - Components Add info commit

--- src/components/ItemElement.js
+++ src/components/ItemElement.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { Link } from '@mui/material';
 
 function ItemElement() {
     return(
-            <div>
-                <p>Item Element</p>
-            </div>
+            <Card sx={{minWidth: 275}}>
+                <CardContent>
+                    <Typography variant='h5' component='div'>frameworkName</Typography>
+                    <Typography variant='h5' component='div'>Lead by leadby</Typography>
+                    <Link href='/' underline='none' component='div'>Official Docs</Link>
+                </CardContent>
+                <CardActions>
+                    <Button variant='outlined' color='success' size='small' href='/qqq'>Edit</Button>
+                    <Button variant='outlined' color='error' size='small' href='/qqq'>Delete</Button>
+                </CardActions>
+            </Card>
     )
 }
 
--- src/components/MainPageHeader.js
+++ src/components/MainPageHeader.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/material/Menu';
+
 
 function MainPageHeader() {
     return(
-        <div>
-                <p>Main page header</p>
-        </div>
+        <Box sx={{ flexGrow: 1}}>
+            <AppBar position='static'>
+                <Toolbar>
+                    <IconButton size='large' edge='start' color='inherit' aria-label='menu' sx={{mr: 2}} href='/'>
+                        <MenuIcon/>
+                    </IconButton>
+                    <Typography variant='h6' component='div' sx={{ flexGrow: 1}}>Javascript Frameworks</Typography>
+                    <Button color='inherit' href='/add'>Add new</Button>
+                </Toolbar>
+            </AppBar>
+        </Box>
     )
 }
 
