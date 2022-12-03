baa566d886d3578b2098ccf485a8dc649bba44b0
pasht <p.tern2298@outlook.com>
2022-12-03 17:08:58 -0500

    - Components commit

--- src/components/ItemElement.js
+++ src/components/ItemElement.js
@@ -6,17 +6,26 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import { Link } from '@mui/material';
 
-function ItemElement() {
+function ItemElement({ id, frameworkName, leadBy, officialDocs }) {
+
+    function deleteFramework() {
+        let frameworks = JSON.parse(localStorage.getItem('frameworks'));
+        if(frameworks != null) {
+            frameworks.splice(id,1);
+            localStorage.setItem('frameworks', JSON.stringify(frameworks));
+            console.log(frameworks);
+        }
+    }
     return(
             <Card sx={{minWidth: 275}}>
                 <CardContent>
-                    <Typography variant='h5' component='div'>frameworkName</Typography>
-                    <Typography variant='h5' component='div'>Lead by leadby</Typography>
-                    <Link href='/' underline='none' component='div'>Official Docs</Link>
+                    <Typography variant='h5' component='div'>${frameworkName}</Typography>
+                    <Typography variant='h5' component='div'>Lead by ${leadBy}</Typography>
+                    <Link href={officialDocs} underline='none' component='div'>Official Docs</Link>
                 </CardContent>
                 <CardActions>
-                    <Button variant='outlined' color='success' size='small' href='/qqq'>Edit</Button>
-                    <Button variant='outlined' color='error' size='small' href='/qqq'>Delete</Button>
+                    <Button variant='outlined' color='success' size='small' href={`/edit/${id}`}>Edit</Button>
+                    <Button variant='outlined' color='error' size='small' href='/' onClick={(event) => { deleteFramework() }}>Delete</Button>
                 </CardActions>
             </Card>
     )
--- src/components/ItemListView.js
+++ src/components/ItemListView.js
@@ -1,13 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import ItemElement from './ItemElement';
+import {Grid} from '@mui/material';
 
 function ItemListView() {
-    return(
-        <div>
-            <div>
-                <p>Item list view</p>
-            </div>
-        </div>
-    )
+
+    let frameworks = JSON.parse(localStorage.getItem('frameworks'))
+    const [frameworkName, setFrameworkName] = useState(null);
+    const [leadBy, setLeadBy] = useState(null);
+    const [officialDocs, setOfficialDocs] = useState(null);
+
+    useEffect( () => {
+        frameworks = JSON.parse(localStorage.getItem('frameworks'))
+        if(frameworks != null) {
+            frameworks.forEach(framework => {
+                setFrameworkName(framework.frameworkName);
+                setLeadBy(framework.leadBy);
+                setOfficialDocs(framework.officialDocs);
+            })
+        }
+    })
+
+    if(frameworks != null) {
+        return (
+            <Grid container justifyContent='center' spacing={3}>
+                {frameworks.map((framework, key) => {
+                    return(
+                        <Grid key={key} item xs={8}>
+                            <ItemElement
+                                id={key}
+                                frameworkName={framework.frameworkName}
+                                leadBy={framework.leadBy}
+                                officialDocs={framework.officialDocs}
+                            />
+                        </Grid>
+                    )
+                })}
+            </Grid>
+        )
+    } else {
+        return(
+            <div>Add some frameworks</div>
+        )
+    }
+
 }
 
 export default ItemListView;
